ca331dc67363931aacf32d1280c55b1d3bb85b1b
vaaluvishnu5146 <vaaluvishnu5146@gmail.com>
2026-06-22 20:31:55 +0530

    Added necessary logic changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,66 @@ function App() {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
   const [cartOpen, setCartOpen] = useState(false);
+  const [cartValue, setCartValue] = useState(0);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    fetch("http://localhost:5173/products.json")
+      .then((response) => response.json())
+      .then((result) => {
+        if (result) {
+          setProducts(result.products);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    const value = cart.reduce((a, b) => {
+      return a + b.price * b.quantity;
+    }, 0);
+    setCartValue(value);
+  }, [cart]);
 
   function toggle() {
     setCartOpen((prev) => !prev);
+  }
+
+  function addToCart(e, data = {}) {
+    const dataEnriched = {
+      ...data,
+      quantity: 1,
+    };
+    const cartCopy = [...cart];
+    cartCopy.push(dataEnriched);
+    setCart(cartCopy);
+  }
+
+  function isItemAddedToCart(id) {
+    const matchingItem = cart.find((item) => item.id === id);
+    if (matchingItem) return true;
+    return false;
+  }
+
+  function cartItemQuantityChange(e, type, id) {
+    let modifiedData = [];
+    if (type === "dec") {
+      modifiedData = cart.map((item) => {
+        if (item.id === id) {
+          item.quantity -= 1;
+        }
+        return item;
+      });
+    } else {
+      modifiedData = cart.map((item) => {
+        if (item.id === id) {
+          item.quantity += 1;
+        }
+        return item;
+      });
+    }
+    setCart(modifiedData);
   }
 
   return (
@@ -25,8 +80,8 @@ function App() {
             <ListItem
               key={`${p.name}-${index}`}
               data={p}
-              addToCart={() => {}}
-              disabled={() => {}}
+              addToCart={addToCart}
+              disabled={isItemAddedToCart(p.id)}
             />
           ))}
         </div>
@@ -34,8 +89,8 @@ function App() {
           isOpen={cartOpen}
           toggle={toggle}
           items={cart}
-          quantityChange={() => {}}
-          totalCartPrice={0}
+          quantityChange={cartItemQuantityChange}
+          totalCartPrice={cartValue}
         />
       </div>
     </>
